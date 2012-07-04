a4ed67daf69e2ea972a4ab59c449bdf1bdb866a6
Jannis <jannisf@gmail.com>
2012-07-04 22:35:25 +0300

    Fix LDFLAGS for windows, glfw now builds on windows

--- glfw.go
+++ glfw.go
@@ -8,7 +8,7 @@ package glfw
 //#cgo   linux LDFLAGS: -L/usr/local/lib -pthread -lX11 -lXrandr -lm -lGL -lrt -lglfw
 //#cgo  darwin  CFLAGS: -I/usr/local/include
 //#cgo  darwin LDFLAGS: -L/usr/local/lib -framework Cocoa -framework OpenGL  -lglfw
-//#cgo windows LDFLAGS: -lglu32 -lopengl32 -glfwdll
+//#cgo windows LDFLAGS: -lglu32 -lopengl32 -lglfwdll
 //#include <stdlib.h>
 //#ifdef _WIN32
 //  #define GLFW_DLL
